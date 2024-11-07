a22e4d29b50fcdcc9e00588cfc7c93268791c27d
Joao Pedro <joaopedrosilvamendes1@gmail.com>
2024-11-07 11:57:00 +0100

    criação das paginas avaliar e cadastrar

--- src/main/java/com/cinemais/cinemais/controller/cinemaisController.java
+++ src/main/java/com/cinemais/cinemais/controller/cinemaisController.java
@@ -1,13 +1,42 @@
 package com.cinemais.cinemais.controller;
 
+import com.cinemais.cinemais.model.filme;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class cinemaisController {
+
+    private List<filme> listaFilmes = new ArrayList();
     
     @GetMapping("/index")
-    public String exibirIndex(){
+    public String exibirIndex() {
         return "index";
+    }
+
+    @GetMapping("/cadastrar")
+    public String exibirCadastrar(Model model) {
+        model.addAttribute("filme", new filme());
+        return "cadastrar";
+    }
+    
+    @PostMapping("/cadastrar")
+    public String processarFormulario(@ModelAttribute filme filme, Model model){
+        listaFilmes.add(filme);
+        model.addAttribute("filme", filme);
+        return "redirect:/avaliar";
+    }
+    
+    @GetMapping("/avaliar")
+    public String exibirAvaliar(Model model){     
+        model.addAttribute("listaFilmes", listaFilmes);
+        return "avaliar";
     }
 }
---
+++ src/main/java/com/cinemais/cinemais/model/filme.java
@@ -0,0 +1,63 @@
+package com.cinemais.cinemais.model;
+
+import java.time.LocalDate;
+
+public class filme {
+
+    private int id;
+    private String titulo;
+    private String sinopse;
+    private String genero;
+    private LocalDate anoLancamento;
+
+    public filme(){
+        
+    }
+    
+    public filme(int id, String titulo,String sinopse, String genero, LocalDate anoLancamento){
+        this.id = id;
+        this.sinopse = sinopse;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public LocalDate getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setAnoLancamento(LocalDate anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+}
